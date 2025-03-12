b192fcb00b0454a025326b6ddf3f9adf072a042c
Telo PHILIPPE <telo.philippe@gmail.com>
2025-03-12 14:52:51 +0100

    Separated tangent space calculations

--- Sources/Mesh.h
+++ Sources/Mesh.h
@@ -12,42 +12,62 @@
 class BVH;
 
 class Mesh : public Transform {
-public:
-    virtual ~Mesh();
+   public:
+	virtual ~Mesh();
 
-    inline const std::vector<glm::vec3>& vertexPositions() const { return m_vertexPositions; }
-    inline std::vector<glm::vec3>& vertexPositions() { return m_vertexPositions; }
-    inline const std::vector<glm::vec3>& vertexNormals() const { return m_vertexNormals; }
-    inline std::vector<glm::vec2>& vertexUVs() { return m_vertexUVs; }
-    inline const std::vector<glm::vec2>& vertexUVs() const { return m_vertexUVs; }
-    inline std::vector<glm::vec3>& vertexNormals() { return m_vertexNormals; }
-    inline const std::vector<glm::vec3>& vertexTangents() const { return m_vertexTangents; }
-    inline std::vector<glm::vec3>& vertexTangents() { return m_vertexTangents; }
-    inline const std::vector<glm::vec3>& vertexBitangents() const { return m_vertexBitangents; }
-    inline std::vector<glm::vec3>& vertexBitangents() { return m_vertexBitangents; }
-    inline const std::vector<glm::uvec3>& triangleIndices() const { return m_triangleIndices; }
-    inline std::vector<glm::uvec3>& triangleIndices() { return m_triangleIndices; }
+	inline const std::vector<glm::vec3>& vertexPositions() const {
+		return m_vertexPositions;
+	}
+	inline std::vector<glm::vec3>& vertexPositions() {
+		return m_vertexPositions;
+	}
+	inline const std::vector<glm::vec3>& vertexNormals() const {
+		return m_vertexNormals;
+	}
+	inline std::vector<glm::vec2>& vertexUVs() { return m_vertexUVs; }
+	inline const std::vector<glm::vec2>& vertexUVs() const {
+		return m_vertexUVs;
+	}
+	inline std::vector<glm::vec3>& vertexNormals() { return m_vertexNormals; }
+	inline const std::vector<glm::vec3>& vertexTangents() const {
+		return m_vertexTangents;
+	}
+	inline std::vector<glm::vec3>& vertexTangents() { return m_vertexTangents; }
+	inline const std::vector<glm::vec3>& vertexBitangents() const {
+		return m_vertexBitangents;
+	}
+	inline std::vector<glm::vec3>& vertexBitangents() {
+		return m_vertexBitangents;
+	}
+	inline const std::vector<glm::uvec3>& triangleIndices() const {
+		return m_triangleIndices;
+	}
+	inline std::vector<glm::uvec3>& triangleIndices() {
+		return m_triangleIndices;
+	}
 
-    inline const std::shared_ptr<BVH> bvh() const { return m_bvh; }
-    inline std::shared_ptr<BVH> bvh() { return m_bvh; }
+	inline const std::shared_ptr<BVH> bvh() const { return m_bvh; }
+	inline std::shared_ptr<BVH> bvh() { return m_bvh; }
 
-    void computeBoundingSphere(glm::vec3& center, float& radius) const;
+	void computeBoundingSphere(glm::vec3& center, float& radius) const;
 
-    void recomputePerVertexNormals(bool angleBased = false);
+	void recomputePerVertexNormals(bool angleBased = false);
 
-    void recomputeUVs(glm::vec2 scale);
+	void recomputeTangentSpace();
 
-    void recomputeBVH(std::shared_ptr<Mesh> meshPtr);
+	void recomputeUVs(glm::vec2 scale);
 
-    void clear();
+	void recomputeBVH(std::shared_ptr<Mesh> meshPtr);
 
-private:
-    std::vector<glm::vec3> m_vertexPositions;
-    std::vector<glm::vec3> m_vertexNormals;
-    std::vector<glm::vec3> m_vertexTangents;
-    std::vector<glm::vec3> m_vertexBitangents;
-    std::vector<glm::vec2> m_vertexUVs;
-    std::vector<glm::uvec3> m_triangleIndices;
+	void clear();
 
-    std::shared_ptr<BVH> m_bvh;
+   private:
+	std::vector<glm::vec3> m_vertexPositions;
+	std::vector<glm::vec3> m_vertexNormals;
+	std::vector<glm::vec3> m_vertexTangents;
+	std::vector<glm::vec3> m_vertexBitangents;
+	std::vector<glm::vec2> m_vertexUVs;
+	std::vector<glm::uvec3> m_triangleIndices;
+
+	std::shared_ptr<BVH> m_bvh;
 };
